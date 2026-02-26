8adbf8857cb882d3db094250361f743637a7dbfd
Ignacio Daniel Araya Zamorano <rilianx@gmail.com>
2026-02-26 11:33:13 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,46 +10,42 @@ testcode="test.c"
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
-#Verifica si se realizaron cambios 
-CHANGED=$(git diff --ignore-space-at-eol -b -w --ignore-blank-lines $target_file)
 
-#Si hubieron cambios se actualiza el log
-if [ -n "$CHANGED" ] || [ ! -f "a.out" ]; then
-  git checkout -- $testcode log &> /dev/null
-  #exec 1>>log 2>&1
+git checkout -- $testcode log &> /dev/null
+#exec 1>>log 2>&1
 
-  echo "$(date): " >> log
-  git diff --stat --ignore-space-at-eol -b -w --ignore-blank-lines $target_file >> log
+echo "$(date): " >> log
+git diff --stat --ignore-space-at-eol -b -w --ignore-blank-lines $target_file >> log
 
-  if ! rm -f a.out 2>/dev/null; then
-      echo "No se pudo eliminar 'a.out'. Por favor elimínalo manualmente." >&3
-      exit 1
-  fi
-  echo "Eliminando 'a.out'..." >&3
-
-  #Compilation
-  echo "Compilando: gcc -g $testcode -Wall -Werror -o a.out" >&3
-  gcc -g $testcode -Wall -Werror -o a.out 2>gcc_err.log
-
-  if [ $? -eq 0 ]; then
-      echo " tests: " $(($(./a.out | grep -c 'OK')))\|$(($(./a.out | grep -c 'FAILED'))) >> log
-      ./a.out | tail -n1 >> log
-      git rev-parse --short HEAD >> log
-      git add $target_file log &> /dev/null 
-      git commit -m 'new try' &> /dev/null 
-  else
-      echo "Complación con errores :c" >&3
-      echo "Compilation failed" >> log
-      cat gcc_err.log >&3   # muestra los errores reales al usuario
-      cat gcc_err.log >> log
-      git rev-parse --short HEAD >> log
-      git add $target_file log &> /dev/null 
-      git commit -m 'new try' &> /dev/null 
-      exit 1
-  fi
-
-
+if ! rm -f a.out 2>/dev/null; then
+    echo "No se pudo eliminar 'a.out'. Por favor elimínalo manualmente." >&3
+    exit 1
 fi
+echo "Eliminando 'a.out'..." >&3
+
+#Compilation
+echo "Compilando: gcc -g $testcode -Wall -Werror -o a.out" >&3
+gcc -g $testcode -Wall -Werror -o a.out 2>gcc_err.log
+
+if [ $? -eq 0 ]; then
+    echo " tests: " $(($(./a.out | grep -c 'OK')))\|$(($(./a.out | grep -c 'FAILED'))) >> log
+    ./a.out | tail -n1 >> log
+    git rev-parse --short HEAD >> log
+    git add $target_file log &> /dev/null 
+    git commit -m 'new try' &> /dev/null 
+else
+    echo "Complación con errores :c" >&3
+    echo "Compilation failed" >> log
+    cat gcc_err.log >&3   # muestra los errores reales al usuario
+    cat gcc_err.log >> log
+    git rev-parse --short HEAD >> log
+    git add $target_file log &> /dev/null 
+    git commit -m 'new try' &> /dev/null 
+    exit 1
+fi
+
+
+
 
 #Retroalimentation
 
